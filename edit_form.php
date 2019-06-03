f18703e66282804265137bd1ca45d966adb4dad1
Self <self@Selfs-MacBook-Pro.local>
2019-06-03 12:43:07 +0300

    added acceptance of all file types because audio files are not being recognized

--- edit_form.php
+++ edit_form.php
@@ -10,7 +10,7 @@ class block_file_edit_form extends block_edit_form
         $mform->setDefault('config_title', get_string('file', 'block_file'));
         $mform->setType('config_title', PARAM_TEXT);
 
-        $mform->addElement('filemanager', 'config_select_file', get_string('configfile', 'block_file'), null, array('subdirs' => false, 'maxfiles' => 5));
+        $mform->addElement('filemanager', 'config_select_file', get_string('configfile', 'block_file'), null, array('subdirs' => false, 'maxfiles' => 5, 'accepted_types'=>'*'));
         $mform->setType('config_select_file', PARAM_RAW);
         $mform->addElement('text', 'config_height', get_string('configheight', 'block_file'));
         $mform->setType('config_height', PARAM_TEXT);
