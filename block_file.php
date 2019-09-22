4ac70705f86b60d2e92d071e7fabd4bde3bf7283
Self <self@Selfs-MacBook-Pro.local>
2019-09-22 15:22:29 +0300

    added leftover files on the tabbing parsing

--- block_file.php
+++ block_file.php
@@ -112,7 +112,7 @@ class block_file extends block_base
         // Add at the end those files that did not match the sorting array
         foreach($files as $file)
         {
-            echo "<script>console.log('$FILENAME ' + '".$file->get_filename()."');</script>";
+            echo "<script>console.log('FILENAME ' + '".$file->get_filename()."');</script>";
 
             if((!array_search($file->get_filename(), $sortedOriginalNames)) && ($file->get_filename()!=".") )
             {
