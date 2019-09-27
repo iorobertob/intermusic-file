57a86fa956984c5bbca75469c24efe447f12cd7d
Self <self@Selfs-MacBook-Pro.local>
2019-09-27 20:22:58 +0300

    print context instance id

--- lib.php
+++ lib.php
@@ -7,7 +7,7 @@ function block_file_pluginfile($course, $birecord_or_cm, $context, $filearea, $a
 
     echo "<string>console.log('PARENT CONTEXT ');</string>";
     echo "<string>console.log('".$parentcontext->instanceid." ');</string>"; 
-    
+    die;
     if ($context->contextlevel != CONTEXT_BLOCK) {
         send_file_not_found();
     }
