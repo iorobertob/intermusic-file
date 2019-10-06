e6387bf9132c7c2c947542699d9fff052f396b92
Self <self@Selfs-MacBook-Pro.local>
2019-10-06 14:40:18 +0300

    add print utility

--- io_print.php
+++ io_print.php
@@ -5,7 +5,7 @@ function file_print($txt)
     global $CFG, $DB;
 
     // $myfile = fopen($CFG->wwwroot."/blocks/file/log.txt", "w") or die("Unable to open file!");
-    $myfile = fopen("/var/www/intermusic.lmta.lt/blocks/file/log.txt", "w") or die("Unable to open file!");
-    fwrite($myfile, $txt."\n") or die('fwrite failed');
-    fclose($myfile);
+    // $myfile = fopen("/var/www/intermusic.lmta.lt/blocks/file/log.txt", "w") or die("Unable to open file!");
+    // fwrite($myfile, $txt."\n") or die('fwrite failed');
+    // fclose($myfile);
 }
