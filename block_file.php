5d4233f1a0545f87c7efd59fee57b9d54669f854
Self <self@Selfs-MacBook-Pro.local>
2019-10-06 15:49:11 +0300

    add print utility

--- block_file.php
+++ block_file.php
@@ -47,6 +47,7 @@ class block_file extends block_base
         }
         require_once("$CFG->dirroot/blocks/file/io_print.php");
         file_print("LOLOLOLO");
+        file_print($data->$file->get_filename());
         $files    = $data->select_file;
             // Add at the end those files that did not match the sorting array
             foreach($files as $file)
