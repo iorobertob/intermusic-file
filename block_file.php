624135971394960d5a0bcac7785df9e9dc5fb1b3
Self <self@Selfs-MacBook-Pro.local>
2019-10-06 16:58:07 +0300

    add print utility

--- block_file.php
+++ block_file.php
@@ -46,22 +46,24 @@ class block_file extends block_base
             }
         }
         require_once("$CFG->dirroot/blocks/file/io_print.php");
-        // file_print("LOLOLOLO");
+        file_print("DEBUG: \n", true);
         // file_print($data->select_file);
-        $files    = $data->select_file;
-        $i = 0;
-        foreach ($data as $item)
-        {
-            file_print($item);
-            $i = $i + 1;
-        }
+        $fs    = get_file_storage();
+        $files = $fs->get_area_files($this->context->id, 'block_file', 'file', 0);
             // Add at the end those files that did not match the sorting array
-            foreach($files as $file)
-            {
-                echo "<script>console.log('".$file -> get_filename()."');</script>";
-                echo $file -> get_filename();
-                // file_print($file -> get_filename());
-            }
+        foreach($files as $file)
+        {
+            // echo "<script>console.log('".$file -> get_filename()."');</script>";
+            // echo $file -> get_filename();
+            file_print($file -> get_filename());
+        }
+
+
+
+
+
+
+
         return parent::instance_config_save($data, $nolongerused);
     }
 
--- io_print.php
+++ io_print.php
@@ -1,11 +1,20 @@
 <?php
 
-function file_print($txt)
+function file_print($txt, $overwrite = false)
 {
     global $CFG, $DB;
 
     // $myfile = fopen($CFG->wwwroot."/blocks/file/log.txt", "w") or die("Unable to open file!");
-    $myfile = fopen("/var/www/intermusic.lmta.lt/blocks/file/log.txt", "a+") or die("Unable to open file!");
+
+    if ($overwrite)
+    {
+        $myfile = fopen($CFG->wwwroot."/blocks/file/log.txt", "w") or die("Unable to open file!"); 
+    }
+    else
+    {
+        $myfile = fopen("/var/www/intermusic.lmta.lt/blocks/file/log.txt", "a+") or die("Unable to open file!");
+    }
+
     fwrite($myfile, $txt."\n") or die('fwrite failed');
     fclose($myfile);
 }
