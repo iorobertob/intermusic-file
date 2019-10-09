7479e48cd8cc95ba05da4af57856e6cc33c10dea
Self <self@Selfs-MacBook-Pro.local>
2019-10-09 14:10:29 +0300

    change print file to create file if it doesnt exist yet

--- io_print.php
+++ io_print.php
@@ -4,17 +4,22 @@ function file_print($txt, $overwrite = false)
 {
     global $CFG, $DB;
 
-    // $myfile = fopen($CFG->wwwroot."/blocks/file/log.txt", "w") or die("Unable to open file!");
+    $path = "$CFG->dirroot/blocks/file/log.txt";
+
+
+    $f = ((file_exists($path))? fopen($path, "a+") : fopen($path, "w+"));
+
 
     if ($overwrite)
     {
-        $myfile = fopen("$CFG->dirroot/blocks/file/log.txt", "w") or die("Unable to open file!"); 
+        // $myfile = fopen($path, "w") or die("Unable to open file!"); 
+        $myfile = ((file_exists($path))? fopen($path, "w+") : fopen($path, "w+")) or die("Unable to open file!"); 
     }
     else
     {
-        $myfile = fopen("$CFG->dirroot/blocks/file/log.txt", "a+") or die("Unable to open file!");
+        // $myfile = fopen($path, "a+") or die("Unable to open file!");
+        $myfile = ((file_exists($path))? fopen($path, "a+") : fopen($path, "w+")) or die("Unable to open file!"); 
     }
-
     fwrite($myfile, $txt."\n") or die('fwrite failed');
     fclose($myfile);
 }
