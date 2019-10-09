568c04a3a08e244538c7676588d37db1cc187245
Self <self@Selfs-MacBook-Pro.local>
2019-10-09 10:36:38 +0300

    get only the last file

--- block_file.php
+++ block_file.php
@@ -47,6 +47,7 @@ class block_file extends block_base
         file_print(gettype($files));
         file_print(sizeof($files));
         file_print("NEXT: \n");
+        file_print($files{1} -> get_filename());
         foreach($files as $file)
         {
             // echo "<script>console.log('".$file -> get_filename()."');</script>";
