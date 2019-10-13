8a7b52bea3f30cdcbbc9e5f20f1647d205649756
Self <self@Selfs-MacBook-Pro.local>
2019-10-13 18:00:34 +0300

    add api call

--- block_file.php
+++ block_file.php
@@ -125,7 +125,7 @@ class block_file extends block_base
 
         $request_json = $this->get_file_fields_metadata($collection[0]);
         file_print($request_json[0], true);
-        file_print($request_json[1][0]['ref']);
+        file_print($request_json[1][]['ref']);
 
         ///////////////////////////////////// \ WHEN SAVING ALTER PARENT A] CTIVITY METADATA ///////////////////////
 
