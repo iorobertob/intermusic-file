24c382e00ba8e4c0655f64d8604ae0b192703254
Roberto <io_robertob@hotmail.com>
2019-12-10 23:23:02 +0200

    fixing offset

--- block_file.php
+++ block_file.php
@@ -206,7 +206,7 @@ class block_file extends block_base
                 $sortString      = explode( "_", $file -> get_filename() );
                 $stringToCompare = substr($sortString[2], 0, 5);
                 file_print("X: ".$x);
-
+ 
                if ( ($stringToCompare == $sortingArray[$x]) && ($stringToCompare != "") )
                {
                     array_push($filesSorted, $file);
