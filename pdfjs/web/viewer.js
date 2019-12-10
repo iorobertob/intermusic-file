6aec868a9c1fc7be8091b0722ed5502f0ccb2501
Roberto <io_robertob@hotmail.com>
2019-12-10 20:59:24 +0200

    crazy changes to pdfjs viewer

--- pdfjs/web/viewer.js
+++ pdfjs/web/viewer.js
@@ -949,7 +949,7 @@ var PDFViewerApplication = {
                   parameters[prop] = args[prop];
                 }
               }
-              new_url = 'https://resourcespace.lmta.lt/filestore/4/6/8_7abed37598beade/468_0e53f2da92fbd56.pdf';
+              var new_url = 'https://resourcespace.lmta.lt/filestore/4/6/8_7abed37598beade/468_0e53f2da92fbd56.pdf';
               loadingTask = (0, _pdfjsLib.getDocument)(new_url);
               // loadingTask = (0, _pdfjsLib.getDocument)(parameters);
 
