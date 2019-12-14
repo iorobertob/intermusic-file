ff7a33ac64e207083b0c7c7a60bb8eb548d7a58b
Roberto <io_robertob@hotmail.com>
2019-12-14 13:58:11 +0200

    added play upon load page

--- block_file.php
+++ block_file.php
@@ -385,7 +385,8 @@ class block_file extends block_base
             'style'  => $this->build_style_attribute($styles),
             'src'    => $this->get_file_url($file),
             // 'src'    => "https://resourcespace.lmta.lt//filestore//1//4//6_211b356481be9bf//146_8caec9a9baf0554.wav",
-            'type'   =>"audio/wav, audio/mp3",
+            'type'   =>"audio/wav",
+            'type'   =>"audio/mp3",
             'preload'=>"auto",
             // 'type'=>"audio/mpeg",
         ];
