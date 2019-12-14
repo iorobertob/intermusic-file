177cb7be0f6f6043c03d5b1f041ed19e1db57457
Roberto <io_robertob@hotmail.com>
2019-12-14 17:43:20 +0200

    added play upon load page

--- block_file.php
+++ block_file.php
@@ -409,10 +409,10 @@ class block_file extends block_base
         $audio_mp3 = $audio_exploded[0].".mp3";
 
 
-
+// <source src="'.$audio_mp3.'" type="audio/mp3"/>
         $tag = '<audio controls="" style="width: 100%">
             <source src="'.$this->get_file_url($file).'" type="audio/x-wav"/>
-            <source src="'.$audio_mp3.'" type="audio/mp3"/>
+            
         </audio>';
         file_print($tag);
 
