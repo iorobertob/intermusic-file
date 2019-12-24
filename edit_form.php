a4952f6c70f34a1da3cb92590ac10617b4987e24
Roberto <io_robertob@hotmail.com>
2019-12-24 20:28:24 +0200

    added set_data in edit form

--- edit_form.php
+++ edit_form.php
@@ -36,6 +36,8 @@ class block_file_edit_form extends block_edit_form
 
         $defaults->config_select_file = $draftitemid;
 
+        file_print(serialize($this->block->config));
+
         // $defaults->file = $draftitemid;
 
         // $this->block->config->attachments = $draftitemid;
@@ -43,7 +45,7 @@ class block_file_edit_form extends block_edit_form
         // $this->block->config_select_file = $draftitemid;
         // unset($this->block->config->attachments); 
 
-        parent::set_data($defaults);
+        // parent::set_data($defaults);
     }
 
 }
