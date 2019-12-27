280ee41c7a606fbe0478c13286d7d4fb51fa8706
Roberto <io_robertob@hotmail.com>
2019-12-27 15:11:47 +0200

    try to make default zoom to full width

--- block_file.php
+++ block_file.php
@@ -322,7 +322,7 @@ class block_file extends block_base
         $attributes1 = [
             'controls' => '',
             'style' => $this->build_style_attribute($styles),
-            'src' => $this->get_file_url($file).'#zoom=1'
+            'src' => $this->get_file_url($file).'#zoom=100'
         ];
         $tag = html_writer::tag('iframe','',$attributes1);
         
