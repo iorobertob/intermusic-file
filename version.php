452e73b4e124b8bc0b1b4dbbab93a8fd2ddff53c
Roberto <iorobertob@gmail.com>
2020-11-08 18:22:24 +0200

    bumped version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_file';
-$plugin->version   = 2018120905;
+$plugin->version   = 2018120906;
 $plugin->requires  = 2016052300;
 $plugin->maturity  = MATURITY_BETA;
 
