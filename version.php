844bf62ea36a8b41f978d2af45cc2e5c4b687b24
Roberto <iorobertob@gmail.com>
2020-11-08 18:09:20 +0200

    added default to config

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_file';
-$plugin->version   = 2018120904;
+$plugin->version   = 2018120905;
 $plugin->requires  = 2016052300;
 $plugin->maturity  = MATURITY_BETA;
 
